c72f020f13c66149250d2d8d8e70edecc68fb5ad
SamuelAbel <1241433@senati.pe>
2022-04-11 07:22:31 -0500

    practica 001 gafico

--- dist/js/colors-chart.js
+++ dist/js/colors-chart.js
@@ -1,14 +1,2 @@
 //arreglos  que contienen el color de fondo y de borde
 
-let listBackground= [
-    'rgba(26, 188, 156,0.3)', 
-    'rgba(231, 76, 60,0.3)',
-    'rgba(97, 106, 107,0.3)',
-    'rgba(185, 119, 14 ,0.3)'
-];
-let listBorder= [
-    'rgba(26, 188, 156,0.3)', 
-    'rgba(231, 76, 60,0.3)',
-    'rgba(97, 106, 107,0.3)',
-    'rgba(185, 119, 14 ,0.3)'
-];  
